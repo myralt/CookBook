42773881dd1be3745aea506067e29a7ec572a01d
myralt <no@email.com>
2022-03-22 09:25:48 +0100

    Skeleton of recipe controller and removed doc for rewrite

--- CookBookAPI/src/Controller/RecipeController.php
+++ CookBookAPI/src/Controller/RecipeController.php
@@ -2,18 +2,67 @@
 
 namespace App\Controller;
 
+use App\Entity\Recipe;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class RecipeController extends AbstractController
 {
-    #[Route('/recipe', name: 'app_recipe')]
-    public function index(): Response
+    #[Route('/add/recipe', name: 'new_recipe')]
+    public function addRecipe(Request $req, SerializerInterface $serializer, ManagerRegistry $doctrine): Response
+    {
+        $form = $serializer->deserialize(
+            $req->getContent(),
+            RecipeForm::class,
+            'json'
+        );
+
+        $entityManager = $doctrine->getManager();
+
+        //$entityManager->persist($newRecipe);
+
+        //$entityManager->flush();
+
+        return new Response(
+            '',
+            Response::HTTP_OK,
+            ['content-type' => 'application/json']
+        );
+    }
+
+    #[Route('/all/recipes', name: 'all_recipes')]
+    public function getAllRecipes(): Response
     {
         return $this->json([
             'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/RecipeController.php',
+        ]);
+    }
+
+    #[Route('/all/recipes/{id}', name: 'recipe')]
+    public function getRecipe(): Response
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+        ]);
+    }
+
+    #[Route('/update/recipe/{id}', name: 'mod_recipe')]
+    public function updateRecipe(): Response
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+        ]);
+    }
+
+    #[Route('/delete/recipe/{id}', name: 'del_recipe')]
+    public function deleteRecipe(): Response
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
         ]);
     }
 }
