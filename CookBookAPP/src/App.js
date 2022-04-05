0b1aa0aefc6672f1b568d31c738ff8d308d98cba
myralt <no@email.com>
2022-04-05 12:15:39 +0200

    Basic reset stylesheet, nav own component

--- CookBookAPP/src/App.js
+++ CookBookAPP/src/App.js
@@ -1,36 +1,12 @@
-import { Link, Outlet } from "react-router-dom";
-import "./App.css";
+import { Outlet } from "react-router-dom";
+import NavBar from "./NavBar";
 
 function App() {
   return (
     <>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/" title="Back to Home">
-              Home
-            </Link>
-          </li>
-          <li>
-            <Link to="/recipes" title="View all recipes">
-              View all recipes
-            </Link>
-          </li>
-          <li>
-            <Link to="/new-recipe" title="New recipe">
-              New Recipe
-            </Link>
-          </li>
-          <li>
-            <Link to="/pantry" title="View pantry">
-              View pantry
-            </Link>
-          </li>
-        </ul>
-      </nav>
+      <NavBar />
       <Outlet />
     </>
   );
 }
-
 export default App;
---
+++ CookBookAPP/src/NavBar.js
@@ -0,0 +1,31 @@
+import { Link } from "react-router-dom";
+
+function NavBar() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/" title="Back to Home">
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link to="/recipes" title="View all recipes">
+            View all recipes
+          </Link>
+        </li>
+        <li>
+          <Link to="/new-recipe" title="New recipe">
+            New Recipe
+          </Link>
+        </li>
+        <li>
+          <Link to="/pantry" title="View pantry">
+            View pantry
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+export default NavBar;
