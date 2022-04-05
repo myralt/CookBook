ebadac80775f430ff8b8681238450511d099d5a3
myralt <no@email.com>
2022-04-05 15:21:23 +0200

    View renders pins retrieved from api

--- CookBookAPP/src/Home.js
+++ CookBookAPP/src/Home.js
@@ -1,4 +1,26 @@
+import { useState, useEffect } from "react";
+
 function Home() {
-  return <h1>Pinned</h1>;
+  const initialState = [{}];
+  const [pins, setPins] = useState(initialState);
+
+  useEffect(() => {
+    const getPins = async () => {
+      const data = await fetch("http://127.0.0.1:8000/pins").then((response) =>
+        response.json()
+      );
+
+      setPins(data);
+    };
+
+    getPins();
+  }, []);
+
+  return (
+    <>
+      <h1>Pinned</h1>
+      <p>{pins[0].name}</p>
+    </>
+  );
 }
 export default Home;
