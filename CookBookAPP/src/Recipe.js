700c9e2258a1d61d5c3dd539bdb8d8ff25a94214
myralt <no@email.com>
2022-04-06 06:21:48 +0200

    Recipe view renders but collection properties are treated as undefined?

--- CookBookAPP/src/Recipe.js
+++ CookBookAPP/src/Recipe.js
@@ -1,5 +1,37 @@
-function Recipe() {
-  return <p>recipe</p>;
-}
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+
+const Recipe = () => {
+  const params = useParams();
+  const [recipe, setRecipe] = useState([]);
+
+  const fetchRecipe = () => {
+    const id = params.recipeId;
+    fetch(`http://127.0.0.1:8000/all/recipes/${id}`)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setRecipe(data);
+      });
+  };
+
+  useEffect(() => {
+    fetchRecipe();
+  }, []);
+
+  return (
+    <>
+      <h1>{recipe.name}</h1>
+      <p>{recipe.creationDate}</p>
+      <p>{recipe.rating}</p>
+      <h2>Ingredients</h2>
+      <h2>Description</h2>
+      <p>{recipe.description}</p>
+      <h2>Preparation</h2>
+      <p>{recipe.preparation}</p>
+    </>
+  );
+};
 
 export default Recipe;
