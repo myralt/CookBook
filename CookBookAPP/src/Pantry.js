ed1a8c5255815c644a4a7c89878cf26a68649963
myralt <no@email.com>
2022-04-06 06:52:35 +0200

    Pantry view renders correctly

--- CookBookAPP/src/Pantry.js
+++ CookBookAPP/src/Pantry.js
@@ -1,9 +1,30 @@
+import { useState, useEffect } from "react";
 import { Link, Outlet } from "react-router-dom";
 
 const Pantry = () => {
+  const [products, setProducts] = useState([]);
+
+  const fetchProducts = () => {
+    fetch("http://127.0.0.1:8000/all/products")
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setProducts(data);
+      });
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
   return (
     <>
-      <p>pantry</p>
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>{product.name}</li>
+        ))}
+      </ul>
       <Link to={"lists"}>View Lists</Link>
       <Outlet />
     </>
